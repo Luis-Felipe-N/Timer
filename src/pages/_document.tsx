024c3dfc2d63bcc39bab9f6602a05c8e987efea9
luis.nunes <luis.nunes@mail.uft.edu.br>
2023-06-20 10:00:55 -0300

    :adhesive_bandage: add crosOrogiin anonymous

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
     <Html lang="pt-BR">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@300;400;500;700&family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet" />
       </Head>
       <body>
